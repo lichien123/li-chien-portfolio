aa6ebc88b42bedd783c6f40b0530f6528b08d2d5
Li Chien <lithal12@gmail.com>
2024-02-02 01:00:16 -0500

    remove ig link

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -2,7 +2,6 @@ import './Socials.css';
 
 import GitHubLogo from '../images/github-orange.svg';
 import LinkedInLogo from '../images/linkedin-orange.svg';
-import InstagramLogo from '../images/instagram-orange.svg';
 
 const Socials = () => {
     return (
@@ -15,11 +14,6 @@ const Socials = () => {
           <li className='social-logo'>
             <a href='https://www.linkedin.com/in/li-chien-163617b1' target='_blank' rel="noreferrer">
               <img src={LinkedInLogo} alt='click here to go to my LinkedIn account!'/>
-            </a>
-          </li>
-          <li className='social-logo'>
-            <a href='https://www.instagram.com/munchiibruh' target='_blank' rel="noreferrer">
-              <img src={InstagramLogo} alt='click here to check out my Instagram!'/>
             </a>
           </li>
         </ul>
