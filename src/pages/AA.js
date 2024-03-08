0ca2e5301436d980f05376d0f280db85af74b959
Li Chien <lithal12@gmail.com>
2024-03-07 23:53:56 -0500

    added more text to account access description

--- src/pages/AA.js
+++ src/pages/AA.js
@@ -17,7 +17,7 @@ const AA = () => {
                 </div>
                 <div className='stat'>
                     <div className='stat-label'>Stack</div>
-                    <div className='stat-data'>React, Redux, GraphQL, React Testing Library, Java, AWS Lambda, AWS S3</div>
+                    <div className='stat-data'>React, Redux, D3, GraphQL, React Testing Library, Java, AWS Lambda, AWS S3</div>
                 </div>
                 <div className='stat'>
                     <div className='stat-label'>Live Site</div>
@@ -30,9 +30,12 @@ const AA = () => {
                 Maintain, debug and enhance the existing application. During the time 
                 I was on the team, our biggest goal was to simplify the on-screen 
                 information in a way a user with minimal investment experience could 
-                understand. I helped create price charts in GraphQL, unified all
+                understand. I helped create price charts D3.js, unified all
                 "as of" dates across the site and simplified business language and 
-                component logic.</div>
+                component logic. Throughout my time maintaining this application, 
+                my ability to read and write code had increased dramtically - I worked 
+                in a fast-paced environment with no senior or junior developers 
+                other than myself.</div>
         </div>
         <Footer/>
     </div>
